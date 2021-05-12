e29270b4f202055a586b86c75fd6edaec0407b43
Nataliya Rohochaya <lc4821577@gmail.com>
2021-05-12 14:45:07 +0300

    feature: home page

---
+++ src/assets/js/home.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".reviews-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
