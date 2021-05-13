78f3fc32e4f051c1db99427b93b511f2cacdb355
Nataliya Rohochaya <lc4821577@gmail.com>
2021-05-13 14:50:35 +0300

    feature: about us page

---
+++ src/assets/js/about-us.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".channel-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 75,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
