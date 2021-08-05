a4260ab9098bb1a3cb5b226dd174ac88c978e503
Nataliya Rohochaya <lc4821577@gmail.com>
2021-08-05 21:04:29 +0300

    fix: background for instagram item

--- src/assets/js/about-us.js
+++ src/assets/js/about-us.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".channel-swiper", {
     slidesPerView: 3,
-    spaceBetween: 75,
+    spaceBetween: 70,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
